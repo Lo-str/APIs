4418b7b7cbcb2e5e0ab5d2484e9cd75b54baf061
LoS <lostreit@pm.me>
2025-12-08 11:26:13 +0100

    added dog generator API, started "Guess Breed" game structure

--- src/pages/API/API.jsx
+++ src/pages/API/API.jsx
@@ -2,30 +2,35 @@ import React, { useState } from "react"
 import style from './API.module.scss'
 
 function API() {
-//     const [joke, setJoke] = useState([])
-//     const fetchData = async () => {// async (asynchronous)= Allow the website   to continue to load while this function runs in the bg
-//         try {
-//             const response = await fetch("https://API-adress.com") // main method when working with API
-//             const data = await response.json() // store all the API data into a variable
-//             setJoke(data)
-//         }
-//         catch (error){
-//             console.log("Error fetching data: ", error)
-//         }
-//     }
-//     // useEffect(() => {fetchData()}, [])
-//     const generateJoke = () => {
-//         fetchData()
-//     }
-     
-//     return(
-//         <div>
-//             <h1>API</h1>
-//             <button onCLick={generateJoke}>Generates joke</button>
-//             <p>Setup: {joke.setup}</p>
-//             <p>Punchline: {}joke.punchline</p>
-//         </div>
-//     )
+    const [dog, setDog] = useState([])
+    const fetchData = async () => {// async (asynchronous)= Allow the website   to continue to load while this function runs in the bg
+        try {
+            const response = await fetch("https://dog.ceo/api/breeds/image/random") // main method when working with API
+            const data = await response.json() // store all the API data into a variable
+            setDog(data)
+        }
+        catch (error){
+            console.log("Error fetching data: ", error)
+        }
+    }
+    // useEffect(() => {fetchData()}, [])
+    const generateDog = () => {
+        fetchData()
+    }
+
+    return(
+      <div>
+        <h1>Guess Breed</h1>
+        <p>Hello! Guess the breed!</p>
+        <img src={dog.message} />
+        <br />
+        <br />
+        <input type="text" placeholder="Type a breed...">
+        </input>
+        <button onClick={generateDog}>Generates dog</button>
+
+      </div>
+    )
 }
 
 export default API
