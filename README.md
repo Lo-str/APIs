8ef975f4379da6dd679096091ddb4824acb1f8e8
LoS <lostreit@pm.me>
2025-12-09 06:57:15 +0100

    started authentication structure

---
+++ README.md
@@ -0,0 +1,27 @@
+Lets finish our final project!
+
+In this assignment you are going to create a registration and login page for your minigame.
+
+Create a registration page
+
+Tasks:
+
+Create a separate page for your registration
+
+It should have username and password as inputs
+
+Add the credentials in the localStorage, one for username and one for password
+
+Feel free to add some requirements as well, for example (must contain a symbol, must contain at least 8 characters and so on)
+
+Create a login page
+
+Tasks:
+
+Create a login page where the user can enter in their login credentials
+
+Make sure that the login information is correct when the user clicks on login
+
+If all the information is correct, the user should be directed to the game page
+
+Feel free to add any amount of CSS
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,42 @@
+import React from "react"
+import {useEffect, useState} from "react"
+import style from './Home.module.scss'
+
 function Home(){
-    return (
-    <div>
-        <h2>Home Page</h2>
-    </div>
+    const [search, setSearch] = useState(" ")
+        const [list, setList] = useState([])
+        
+        const addStorage = () => {
+            localStorage.setItem("key", search)
+            setSearch(" ")
+            setList((l) => [...l, localStorage.getItem("key")])
+        }
+    
+        const clearList = () => {
+            localStorage.clear()
+        }
+        useEffect(() => {
+            setList([localStorage.getItem("key")])
+        }, [])
+        
+        return(
+            <div>
+                <h1>Home Page</h1>
+                <br />
+                <input placeholder="Username..." type="text" value={search} onChange={(e) => setSearch(e.target.value)} OnClick={addStorage} />
+                <br />
+                <br />
+                <input type="text" value={search} placeholder="Password..." onChange={(e) => setSearch(e.target.value)} OnClick={addStorage} />
+                <br />
+                <br />
+                <button>Sign Up</button>
+                {/* <button onClick={clearList}>Clear List</button> */}
+                {/* <ul>
+                    {list.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul> */}
+            </div>
     )
 }
 
